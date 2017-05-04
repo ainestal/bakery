a28d82aefebb1861d51ea7277ac6e0100e09c787
Alejandro Inestal <ainestal@gmail.com>
2017-05-04 22:15:06 +0100

    Send and receive

---
+++ assistant/assistant.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import datetime
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    print("%r - Received %r" % (st, body))
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,
+                      queue='task_queue')
+
+channel.start_consuming()
--- client/client.py
+++ client/client.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 import pika
+import sys
+import time
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+COUNTER=0
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+
+while True:
+    COUNTER += 1
+    message = ' '.join(sys.argv[1:]) or str(COUNTER) + " 2 Brownies"
+    channel.basic_publish(exchange='',
+                          routing_key='task_queue',
+                          body=message,
+                          properties=pika.BasicProperties(
+                              delivery_mode = 2, # make message persistent
+                          ))
+    print(" Sent %r" % message)
+    time.sleep(1)
 
 connection.close()
