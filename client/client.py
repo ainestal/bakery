6e7953654d209a450189da7e0dae35e9d044c5df
Alejandro Inestal <ainestal@gmail.com>
2017-05-05 11:30:23 +0100

    Insert in redis when order sent

--- client/client.py
+++ client/client.py
@@ -4,7 +4,9 @@ import pika
 import sys
 import time
 
-COUNTER=0
+from redis import Redis
+
+redis = Redis(host='redis', port=6379)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
@@ -12,18 +14,22 @@ channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
 
 
+COUNTER=0
 while True:
     COUNTER += 1
-    message = ' '.join(sys.argv[1:]) or str(COUNTER) + " 2 Brownies"
+    message = ' '.join(sys.argv[1:]) or str(COUNTER) + " 1 Brownie"
     channel.basic_publish(exchange='',
                           routing_key='task_queue',
                           body=message,
                           properties=pika.BasicProperties(
                               delivery_mode = 2, # make message persistent
                           ))
+
+    redis.set("orders", str(COUNTER) + " Brownies")
+
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    print("%r -  Sent %r" % (st, message))
+    print("%r -  Sent and saved %r" % (st, message))
     time.sleep(1)
 
 connection.close()
