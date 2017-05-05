33a6d7ee18a7ed8bf2810afdd2b33cabf82af7a7
Alejandro Inestal <ainestal@gmail.com>
2017-05-05 12:20:04 +0100

    Insert in InfluxDB

--- client/client.py
+++ client/client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import datetime
+from influxdb import InfluxDBClient
 import pika
 import sys
 import time
@@ -7,8 +8,28 @@ import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
-
 channel.queue_declare(queue='task_queue', durable=True)
+
+
+client = InfluxDBClient('influxdb', 8086, 'root', 'root', 'bakery')
+client.create_database('bakery')
+
+def create_measurement():
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    return [
+        {
+            "measurement": "orders",
+            "tags": {
+                "from": "client",
+                "to": "assistant"
+            },
+            "time": st,
+            "fields": {
+                "value": 1
+            }
+        }
+    ]
 
 
 COUNTER=0
@@ -21,10 +42,10 @@ while True:
                           properties=pika.BasicProperties(
                               delivery_mode = 2, # make message persistent
                           ))
+    measurement = create_measurement()
+    client.write_points(measurement)
 
-    ts = time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    print("%r -  Sent and saved %r" % (st, message))
+    print("Sent and saved %r" % message)
     time.sleep(1)
 
 connection.close()
